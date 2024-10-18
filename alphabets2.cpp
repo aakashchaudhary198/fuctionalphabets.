1606cfb0c144e39cf3c006a01e7826dd22037901
aakashchaudhary198 <aakashchoudhary016@gmail.com>
2024-10-18 12:37:02 +0530

    Add files via upload

---
+++ alphabets2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void alpha(char);
+void alpha(char ch)
+{
+	switch(ch)
+	case'a':
+		case'e':
+			case'i':
+				case'o':
+					case'u':
+		printf(" is vowel");
+		defalt:
+			printf("is content");
+}
+int main()
+ {
+ 	char ch;
+ 	 printf(" enter the alphabets");
+ 	 scanf("%c",&ch);
+ 	 alpha(ch);
+ 	 return 0;
+ }
